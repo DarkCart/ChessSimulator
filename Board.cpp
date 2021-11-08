dd507261747fb879a78b51d43cfa4fd86fc97625
John <minecart94@gmail.com>
2021-11-08 11:13:25 -0600

    horiffic board rendering code

--- Board.cpp
+++ Board.cpp
@@ -1,15 +1,20 @@
 #include "Board.h"
 
+#define TRANSPARENT 0
+#define WHITE 97
+#define BLACK 30
+
 void Board::initBoard() {
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
-            if (i % 2 == 0 && j % 2 != 0 || i % 2 != 0 && j % 2 == 0) {
-                board[i][j] = 'X';
-            } else {
-                board[i][j] = '0';
-            }
+            board[j][i] = ' ';
+            colors[j][i] = 0;
         }
     }
+
+    updateBoard('a', 1, 'p', WHITE);
+    updateBoard('a', 2, 'K', BLACK);
+    updateBoard('c', 1, 'Q', TRANSPARENT);
 }
 
 void Board::drawBoard() {
@@ -18,14 +23,30 @@ void Board::drawBoard() {
     for (int i = 0; i < 8; i++) {
         std::cout << "┃";
         for (int j = 0; j < 8; j++) {
-            if (board[i][j] == 'X') {
-                std::cout << "\x1b[100m";
-            } else if (board[i][j] == '0') {
-                std::cout << "\x1b[47m";
+            std::string ansi_string = "";
+            bool darkSquare = false;
+
+            if (i % 2 == 0 && j % 2 != 0 || i % 2 != 0 && j % 2 == 0) {
+                darkSquare = true;
             }
-            std::cout << " \x1b[0m┃";
+
+            ansi_string += (darkSquare) ? "\x1b[41" : "\x1b[101";
+            
+            if (colors[j][i] != 0) {
+                ansi_string += ";" + std::to_string(colors[j][i]);
+            } else {
+                ansi_string += (darkSquare) ? ";31" : ";91";
+            }
+            
+            ansi_string += "m";
+            std::cout << ansi_string << board[j][i] << "\x1b[0m┃";
         }
         std::cout << " " << (i + 1) << std::endl;
     }
     std::cout << "┗━┻━┻━┻━┻━┻━┻━┻━┛" << std::endl;
+}
+
+void Board::updateBoard(char file, int rank, char newChar, int color) {
+    board[file - 97][rank - 1] = newChar;
+    colors[file - 97][rank - 1] = color;
 }
--- Board.h
+++ Board.h
@@ -4,6 +4,8 @@ class Board {
     public:
         void initBoard();
         void drawBoard();
+        void updateBoard(char file, int rank, char newChar, int color);
     private:
         char board[8][8];
+        int colors[8][8];
 };
